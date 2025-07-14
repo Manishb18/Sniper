03edb310adb45f22f509cbcf8e3670112f5499c7
Manish Bonala <bonalamanish@gmail.com>
2025-07-14 10:48:56 +0530

    Update server.js - CORS

--- backend/server.js
+++ backend/server.js
@@ -11,13 +11,22 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(cors());
+app.use(cors({
+  origin: "https://sniper-phi.vercel.app",
+  credentials: true
+}));
+app.options('*', cors({
+  origin: "https://sniper-phi.vercel.app",
+  credentials: true
+}));
+
 app.use(express.json());
 
 // Routes
-app.use('/', redirectRoutes);
 app.use('/api', urlRoutes);
 app.use('/api/auth', authRoutes);
+app.use('/', redirectRoutes);
+
 
 const uri = process.env.MONGO_URI || "";
 
